b8006545d4dcab4fb1d16a9c604cb560835e4013
Ohad Forman <ohadforman@mail.tau.ac.il>
2023-05-13 20:29:10 +0300

    incnichols

--- Matlab/Control_system/invnichols.m
+++ Matlab/Control_system/invnichols.m
@@ -5,19 +5,21 @@ nichols(g);
 
 plot_handle = gca;
 [nicgrid, nictext] = invnicchart(plot_handle);
-axis([-270,0,-40,40]);
+axis([-270,0,-80,80]);
 set(nictext, 'Visible', 'off');
 
 title_handle = get(plot_handle, 'Title');
 if(strcmp(get(title_handle, 'String'),''))
-	for i = 1:12
-		position = get(nictext(i), 'Position');
-		position(2) = -1 * position(2);
-		set(nictext(i), 'Position', position);
-	end
+    if numel(nictext) >= 12 % add check for number of elements
+        for i = 1:12
+            position = get(nictext(i), 'Position');
+            position(2) = -1 * position(2);
+            set(nictext(i), 'Position', position);
+        end
 
-	set(nictext, 'Visible', 'on');
-
-	title('');
-	set(title_handle, 'String', 'Inverse Nichols Chart');
+        set(nictext, 'Visible', 'on');
+    end
+    
+    title('');
+    set(title_handle, 'String', 'Inverse Nichols Chart');
 end
